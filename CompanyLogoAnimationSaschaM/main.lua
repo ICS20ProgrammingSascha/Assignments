8a6138bd38fc3d8006bc8e7122c21fba300eeee6
saschamotz <sascha.motz@ocsbstudent,ca>
2018-11-07 13:31:01 -0500

    images

--- CompanyLogoAnimationSaschaM/main.lua
+++ CompanyLogoAnimationSaschaM/main.lua
@@ -1,7 +1,12 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---
------------------------------------------------------------------------------------------
+-- Title: CompanyLogoAnimation
+-- Name: Sascha Motz
 
--- Your code here
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--add the logo image
+local logo = display.newImageRect("Images/CompanyLogoSaschaM.png", 200, 200)
+
+--set initial x and y position of the logo
+logo.x = display.contentCenter
+logo.y = display.contentCenter
